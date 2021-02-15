a2f901b7d143b205e340219d9af6616335a000fe
aniktanims <anik.rtsb@gmail.com>
2021-02-16 00:08:42 +0600

    Fixed All Issues, and Added Features

--- script.js
+++ script.js
@@ -1,24 +1,47 @@
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
-const score = 20;
+"use strict";
+
+const displayMessage = function (message) {
+  document.querySelector(".guess").textContent = message;
+};
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
 document.querySelector(".score").textContent = score;
 
 document.querySelector(".check").addEventListener("click", function () {
   const inputNumber = Number(document.querySelector(".input_number").value);
   if (!inputNumber) {
-    document.querySelector(".guess").textContent = "⛔ No Input Number!";
+    displayMessage("⛔ No Input Number!");
   } else if (inputNumber === secretNumber) {
-    document.querySelector(".guess").textContent = "🎉 Correct Number!";
+    displayMessage("🎉 Correct Number!");
     document.querySelector(".card").style.backgroundColor = "#3e971f";
     document.querySelector(".what").textContent = secretNumber;
-  } else if (inputNumber < secretNumber) {
-    document.querySelector(".guess").textContent = "🔻↙ Too Low";
-    document.querySelector(".card").style.backgroundColor = "#452161";
-    score = -1;
-    document.querySelector(".score").textContent = score;
-  } else if (inputNumber > secretNumber) {
-    document.querySelector(".guess").textContent = "↗ Too High🙃";
-    document.querySelector(".card").style.backgroundColor = "#7c1d58";
-    score = -1;
-    document.querySelector(".score").textContent = score;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (inputNumber != secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        inputNumber > secretNumber ? "↗ Too High🙃" : "🔻↙ Too Low"
+      );
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("Game Over! 🦆");
+
+      alert("Stupid, You don't have brain! 🐒");
+      document.querySelector(".score").textContent = 0;
+    }
   }
+});
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector(".input_number").value = "";
+  document.querySelector(".what").textContent = "?";
+  document.querySelector(".card").style.backgroundColor = "black";
 });
